a00db5ae81eafc15fa856f708082649d4a61a0af
Juan Camilo <jcamilovelandiab@gmail.com>
2020-09-07 22:59:38 -0500

    Updates

---
+++ precache-manifest.f6cfe1e778b7b16085bddcc7a2466cfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eecd6d1f6694d463e87dbcdcfb03c606",
+    "url": "/monsters-rolodex-reactjs-udemy/index.html"
+  },
+  {
+    "revision": "239faecdf24f7c3871d4",
+    "url": "/monsters-rolodex-reactjs-udemy/static/css/main.56ef25d7.chunk.css"
+  },
+  {
+    "revision": "434efb8dc3742223bfb6",
+    "url": "/monsters-rolodex-reactjs-udemy/static/js/2.397cbbff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-reactjs-udemy/static/js/2.397cbbff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "239faecdf24f7c3871d4",
+    "url": "/monsters-rolodex-reactjs-udemy/static/js/main.8917adba.chunk.js"
+  },
+  {
+    "revision": "0ecfe6c7e63b71c45e02",
+    "url": "/monsters-rolodex-reactjs-udemy/static/js/runtime-main.1ba431f7.js"
+  }
+]);
